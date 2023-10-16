0fb5858230a014c099bfc5e5bcc7dbe6580f5d08
Diego-TD <dtd2005@hotmail.com>
2023-10-15 20:05:28 -0700

    CinemaAlgorithm prev 1

--- src/main/java/CinemaAlgorithm.java
+++ src/main/java/CinemaAlgorithm.java
@@ -2,85 +2,143 @@ import java.util.Scanner;
 
 public class CinemaAlgorithm {
     public void CategoryChecker(){
-        byte people, ageOne, studentOne, ageTwo, studentTwo;
-        String categories, ticketOne, ticketTwo;
-        float money; // apparently it is unnecessary
+        byte people, ageOne, studentOne, ageTwo, studentTwo, total, end = 0;
+        String categories;
         Scanner scan = new Scanner(System.in);
 
-        // people filter
-        System.out.println("Introduce the number of people (1 or 2 only):");
-        people = scan.nextByte();
+        while (end == 0) {
+            studentOne = 1;
+            studentTwo = 1;
+            // people filter
+            System.out.println("Introduce the number of people (1 or 2 only):");
+            people = scan.nextByte();
 
-        if (people < 1 || people > 2) {
-            System.out.println("Only 1 or 2 >:)");
-            return;
-        }
-
-        // clean buffer?
-
-        if (people == 1){
-            System.out.println("Introduce the age of the person: (a number)");
-            ageOne = scan.nextByte();
-
-            if (ageOne < 16) {
-                System.out.println("Does 'it' has a student credential? Type a number: \n True: 0 \n False: 1");
-                studentOne = scan.nextByte();
-                if (studentOne < 0 || studentOne > 1){
-                    System.out.println("That is not a valid option (student credential)");
-                    return ;
-                }
-            }
-
-            switch (ageOne){
-                ///
-            }
-
-            if (ageOne < 13) {
-                categories = "G";
-                if (studentOne == 0){
-                    ticketOne = "$35";
-                    System.out.println("You can watch movies with these categories: " + categories + ". Your ticket would cost: " + ticketOne);
-                } else {
-                    ticketOne = "$60";
-                    System.out.println("You can watch movies with these categories: " + categories + ". Your ticket would cost: " + ticketOne);
-                }
-            } else if (ageOne >= 13 && ageOne <16) {
-                categories = "G, PG";
-                if (studentOne == 0){
-                    ticketOne = "$35";
-                    System.out.println("You can watch movies with these categories: " + categories + ". Your ticket would cost: " + ticketOne);
-                } else {
-                    ticketOne = "$60";
-                    System.out.println("You can watch movies with these categories: " + categories + ". Your ticket would cost: " + ticketOne);
-                }
-            } else {
-                categories = "G, PG, R";
-                ticketOne = "$60";
-                System.out.println("You can watch movies with these categories: " + categories + ". Your ticket would cost: " + ticketOne);
-            }
-
-
-        } else { // 2
-            System.out.println("Introduce the information of the first person: \n Introduce the age of the person: (a number)");
-            ageOne = scan.nextByte();
-            System.out.println("Does 'it' has a student credential? Type a number: \n True: 0 \n False: 1");
-            studentOne = scan.nextByte();
-
-            System.out.println("Introduce the information of the second person: \n Introduce the age of the person: (a number)");
-            ageTwo = scan.nextByte();
-            System.out.println("Does 'it' has a student credential? Type a number: \n True: 0 \n False: 1");
-            studentTwo = scan.nextByte();
-
-            if (studentOne < 0 || studentOne > 1 && studentTwo < 0 || studentTwo > 1){
-                System.out.println("That is not a valid option (student credential)");
+            if (people < 1 || people > 2) {
+                System.out.println("Only 1 or 2 >:)");
                 return;
             }
 
+            if (people == 1) {
+                System.out.println("Introduce the age of the person: (a number)");
+                ageOne = scan.nextByte();
 
+                System.out.println("Does 'it' has a student credential? Type a number: \n True: 0 \n False: 1");
+                studentOne = scan.nextByte();
+                if (studentOne < 0 || studentOne > 1) {
+                    System.out.println("That is not a valid option (student credential)");
+                    return;
+                }
+
+                if (ageOne < 13) {
+                    categories = "G";
+                    if (studentOne == 0) {
+                        total = 35;
+                        System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                    } else {
+                        total = 60;
+                        System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                    }
+                } else if (ageOne <= 16) {
+                    categories = "G, PG";
+                    if (studentOne == 0) {
+                        total = 35;
+                        System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                    } else {
+                        total = 60;
+                        System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                    }
+                } else {
+                    if (studentOne == 0) {
+                        System.out.println("Too old to be an student :) | You don't have any discount, sorry.");
+                    }
+                    categories = "G, PG, R";
+                    total = 60;
+                    System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                }
+
+            } else { // 2
+                System.out.println("Introduce the information of the first person: \n Introduce the age of the person: (a number)");
+                ageOne = scan.nextByte(); // duplicated, people can be a class? with a person as objects :0
+                if (ageOne < 17) {
+                    System.out.println("Does 'it' has a student credential? Type a number: \n True: 0 \n False: 1");
+                    studentOne = scan.nextByte();
+                    if (studentOne < 0 || studentOne > 1) {
+                        System.out.println("That is not a valid option (student credential)");
+                        return;
+                    }
+                }
+
+                System.out.println("Introduce the information of the second person: \n Introduce the age of the person: (a number)");
+                ageTwo = scan.nextByte(); // duplicated, people can be a class? with a person as objects :0
+                if (ageTwo < 17) {
+                    System.out.println("Does 'it' has a student credential? Type a number: \n True: 0 \n False: 1");
+                    studentTwo = scan.nextByte();
+                    if (studentTwo < 0 || studentTwo > 1) {
+                        System.out.println("That is not a valid option (student credential)");
+                        return;
+                    }
+                }
+
+                if (ageOne >= 17 || ageTwo >= 17) {
+                    if (ageOne < 13 || ageTwo < 13) {
+                        categories = "G, PG";
+                        if (studentOne == 0 || studentTwo == 0) {
+                            total = 95;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        } else {
+                            total = 120;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        }
+                    } else {
+                        categories = "G, PG, R";
+                        if (ageOne >= 17 && ageTwo >= 17) {
+                            total = 120;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        } else {
+                            if (studentOne == 0 || studentTwo == 0) {
+                                total = 95;
+                                System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                            } else {
+                                total = 120;
+                                System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                            }
+                        }
+                    }
+
+                } else {
+                    if (ageOne < 13 || ageTwo < 13) {
+                        categories = "G";
+                        if (studentOne == 0 && studentTwo == 0) {
+                            total = 70;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        } else if (studentOne == 0 || studentTwo == 0) {
+                            total = 95;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        } else {
+                            total = 120;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        }
+                    } else {
+                        categories = "G, PG";
+                        if (studentOne == 0 && studentTwo == 0) {
+                            total = 70;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        } else if (studentOne == 0 || studentTwo == 0) {
+                            total = 95;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        } else {
+                            total = 120;
+                            System.out.println("You can watch movies with these categories: " + categories + ". The total is: $" + total);
+                        }
+                    }
+                }
+            }
+            System.out.println("\nDid you finish? \n FINISH: 1 \n CONTINUE: 0 ");
+            end = scan.nextByte(); // It crashes if you type a character :(
+            if (end != 0 && end != 1) {
+                System.out.println("xd");
+                end = 0;
+            }
         }
-
-        // clean buffer?
-
-
     }
 }
